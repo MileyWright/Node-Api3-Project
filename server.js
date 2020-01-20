8d706dc5f5cd22452c5f009679900c406bf72030
Miley Wright <dishaliekwright@yahoo.com>
2020-01-20 18:14:58 -0500

    set up logger middleware

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use(express.json())
 //custom middleware
+server.use(logger);
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const {method, originalUrl} = req;
+  console.log( ` ${method} to ${originalUrl} at [${new Date().toISOString()}]`)
+  next();
+};
+
+
 
 module.exports = server;
