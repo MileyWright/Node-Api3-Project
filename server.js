0a0c50726e46e1f36e5a6097046de6236e47c6ab
Miley Wright <dishaliekwright@yahoo.com>
2020-01-20 18:44:20 -0500

    set up server routes to the user and posts

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter')
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use(express.json())
-//custom middleware
+server.use(express.json());
 server.use(logger);
+server.use('/api/user', userRouter)
+server.user('/api/posts', postRouter)
+
 
 function logger(req, res, next) {
   const {method, originalUrl} = req;
