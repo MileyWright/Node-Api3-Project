2c959d5822862c9301e63205346c383fee1f65b5
Miley Wright <dishaliekwright@yahoo.com>
2020-01-20 22:49:00 -0500

    created all custom middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const userDb = require('./userDb');
+router.user(express.json());
 
 router.post('/', (req, res) => {
   // do your magic!
@@ -33,15 +35,37 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  userDb.getById(id)
+  .then(post => {
+    if(!post) {
+      res.status(404).json({error: 'The specified ID does not exist.'})
+    } else {
+      next();
+    }
+  })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const data = req.body;
+  if(!data){
+    res.status(400).json({message: "missing user data."})
+  } else if(!data.name){
+    res.status(400).json({message: "missing required name field."})
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const data = req.body;
+  if(!data){
+    res.status(400).json({message: 'missing post data.'})
+  } else if(!data.text){
+    res.status(400).json({message: "missing required text field."})
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
