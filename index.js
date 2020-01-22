fd40d3cc3cba4fee1613611fbb056c93f2cc9723
Miley Wright <dishaliekwright@yahoo.com>
2020-01-22 18:45:50 -0500

    introduced process.env and dotenv

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server');
 
-server.listen(5000, () => {
-    console.log("Server is running! ^.^")
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${post} ! ^.^`)
 })
