cba0639e67afb8a11f738f9de8ba1fa3425299b2
Miley Wright <dishaliekwright@yahoo.com>
2020-01-22 21:23:39 -0500

    Port is set up correctly

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./server');
 const port = process.env.PORT;
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${post} ! ^.^`)
+    console.log(`Server is running on http://localhost:${port} ^.^`)
 })
